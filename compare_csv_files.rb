09f105467670d89031c2fe99fae682667e874300
Will Prescott <will@theprescotts.com>
2021-05-23 15:21:34 -0500

    Enhance algorithm to look at common rows and common columns, then run both field and record diffs.

--- compare_csv_files.rb
+++ compare_csv_files.rb
@@ -7,46 +7,44 @@ require 'csv'
 ### ##################################################################################
 def main
   params = process_command_line_arguments
-  request_type = params['request_type']
   ignore = params['ignore']
   key_name = params['key_name']
   old_file_path = params['old_file_path']
   new_file_path = params['new_file_path']
   old_table = read_data(old_file_path)
   new_table = read_data(new_file_path)
-  ignore
-  print "\n"
-  print "Running —#{request_type}— difference\n"
-  case request_type
-  when 'fields'
-    difference_fields(old_file_path, new_file_path, old_table, new_table, ignore, key_name)
-  when 'records'
-    difference_records(old_file_path, new_file_path, old_table, new_table, key_name)
-  else
-    print_usage
-  end
+  common_rows = difference_records(old_file_path, new_file_path, old_table, new_table, key_name)
+  difference_fields(old_file_path, new_file_path, old_table, new_table, ignore, key_name, common_rows)
   print "\n"
 end
 
 ### ##################################################################################
-def difference_fields(old_file_path, new_file_path, old_table, new_table, ignore, key_name)
-  print "Old file: #{old_file_path}\n"
-  print "  Number of rows old: ", old_table.length, "\n"
-  print "New file: #{new_file_path}\n"
-  print "  Number of rows new: ", new_table.length, "\n"
-  print "\n"
-  if old_table.length != new_table.length
-    print "Table lengths differ, exiting.\n"
+def create_hash(table, key_name)
+  hash_table = {}
+  table.by_row!()
+  (0...table.length).each do |index|
+#     print 'table[index][key_name]: ', table[index][key_name],"\n"
+#     print 'table[index]: ', table[index].to_h,"\n"
+    hash_table[table[index][key_name]] = table[index].to_h
   end
+  return hash_table
+end
+
+### ##################################################################################
+def difference_fields(old_file_path, new_file_path, old_table, new_table, ignore, key_name, common_rows)
+  common_columns = old_table.headers & new_table.headers
+  old_hash = create_hash(old_table, key_name)
+  new_hash = create_hash(new_table, key_name)
+  print "Common columns: #{common_columns.length}\n"
   print "Differences:\n"
-  old_table.headers.each do |header|
-    next if ignore.include?(header)
-    (0..old_table.length).each do |index|
-      if old_table[header][index] != new_table[header][index]
-        print "record: #{old_table[key_name][index] }, field: #{header}\n"
-        print "  old_value: ->#{old_table[header][index]}, '<-\n"
-        print "  new_value: ->#{new_table[header][index]}, '<-\n"
-        end
+  common_columns.each do |column|
+    next if ignore.include?(column)
+    common_rows.each do |row|
+      if old_hash[row][column] != new_hash[row][column]
+        print "record: #{row}, field: #{column}\n"
+        print "  old_value: ->#{old_hash[row][column]}<-\n"
+        print "  new_value: ->#{new_hash[row][column]}<-\n"
+      end
     end
   end
 end
@@ -58,7 +56,7 @@ def difference_records(old_file_path, new_file_path, old_table, new_table, key_n
   new_old = new_array - old_array
 
   old_new = old_array - new_array
-  intersection = old_array & new_array
+  common_rows = old_array & new_array
 
   print "Old file: #{old_file_path}\n"
   print "  Old count: #{old_array.length}\n"
@@ -66,15 +64,16 @@ def difference_records(old_file_path, new_file_path, old_table, new_table, key_n
   print "New file: #{new_file_path}\n"
   print "  New count: #{new_array.length}\n"
 
-  print_record_differences('Losses', old_new)
-  print_record_differences('Gains',  new_old)
-  print_record_differences('Intersection', intersection)
+  print_record_differences('Losses: ', old_new)
+  print_record_differences('Gains: ',  new_old)
+  print_record_differences('Common rows: ', common_rows)
+  return common_rows
 end
 
 ### ##################################################################################
 def print_record_differences(type, id_list)
   print type, ": ", id_list.length, "\n"
-  print "  ", id_list, "\n" if type != 'Intersection'
+  print "  ", id_list, "\n" if type != 'Common rows: '
 end
 
 ### ##################################################################################
@@ -88,11 +87,9 @@ def print_usage
   print "Options:\n"
   print "  -1, --first    - first_file_path - first file (required)\n"
   print "  -2, --second   - second_file_path - second file (required)\n"
-  print "  -f, --fields   - find differences in fields (default)\n"
   print "  -h, --help     - print these notes\n"
   print "  -i, --ignore   - comma separated list of field names to ignore when differencing (no_spaces)\n"
   print "  -k, --key_name - name of column containing a unique id (required for records, defaults to 'id')\n"
-  print "  -r, --records  - find differences in records \n"
   print "\n"
   print "Note 0: files must have a header row including the name of the key field.\n"
   print "Note 1: if -r and -f are both specified the last one is used.\n"
@@ -118,22 +115,16 @@ def process_command_line_arguments
       index += 1
       new_file_path = ARGV[index]
       index += 1
-    when '-f', '--fields'
-      request_type = 'fields'
-      index += 1
     when '-h', '--help'
       print_usage
       exit
     when '-i', '--ignore'
       index += 1
-      ignore = ARGV[index].split(/,/)
+      ignore = ARGV[index].gsub(/\s/,'').split(/,/)
       index += 1
     when '-k', '--key_name'
       index += 1
       key_name = ARGV[index]
-      index += 1
-    when '-r', '--records'
-      request_type = 'records'
       index += 1
     else
       print_usage
@@ -149,7 +140,6 @@ def process_command_line_arguments
   key_name = key_name.nil? ? 'id' : key_name
   ignore = ignore.nil? ? [] : ignore
   return {
-    'request_type' => request_type,
     'key_name' => key_name,
     'ignore' => ignore,
     'old_file_path' => old_file_path,
